9a2aa94ff0bef4508a2e0687dc064dfc51086ac8
Ben Draut <drautb@gmail.com>
2017-05-25 17:01:35 -0600

    Get organized for unit tests

---
+++ subc/src/test/parser_test.ml
@@ -0,0 +1,11 @@
+open OUnit2
+
+let parser_tests =
+  "Parser tests">:::
+  [
+    "declarations">::
+    (fun test_ctxt ->
+      todo "Getting set up...";
+      assert_bool "testing" false);
+  ]
+;;
---
+++ subc/src/test/tests.ml
@@ -0,0 +1,6 @@
+open OUnit2
+open Parser_test
+
+let () =
+  run_test_tt_main parser_tests
+;;
