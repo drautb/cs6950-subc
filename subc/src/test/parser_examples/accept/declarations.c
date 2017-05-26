0c1318de090d7981e872516ff791772b9092e17d
Ben Draut <drautb@gmail.com>
2017-05-26 06:36:27 -0600

    Simple framework for testing parser
    * When using Oasis, to reference modules in a different directory, you
      have to build a library, and then reference that. (Oasis doesn't
      support manually adding `-I` args)
    * Parser tests automatically validate all programs in
      `parser_examples/(accept|reject)`.

---
+++ subc/src/test/parser_examples/accept/declarations.c
@@ -0,0 +1,13 @@
+int x;
+int* px;
+int** ppx;
+
+int a[10];
+int* a[10];
+
+char c;
+char* pc;
+char** ppc;
+
+char ca[100];
+char* pca[100];
--- subc/src/test/parser_test.ml
+++ subc/src/test/parser_test.ml
@@ -1,11 +1,39 @@
 open OUnit2
+open Core
+
+let load_programs path =
+  let base = (Sys.getcwd ()) ^ "/parser_examples/" ^ path ^ "/" in
+  let file_list = Sys.readdir base in
+  List.map (Array.to_list file_list)
+    ~f:(fun filename ->
+        let file = In_channel.create (base ^ filename) in
+        let contents = In_channel.input_all file in
+        let test_case = (filename, contents) in
+        In_channel.close file;
+        test_case)
+;;
+
+let parse_program prog_str =
+  Parser.program Lexer.read (Lexing.from_string prog_str)
+;;
+
+let accepted_programs = load_programs "accept"
+let rejected_programs = load_programs "reject"
 
 let parser_tests =
-  "Parser tests">:::
-  [
-    "declarations">::
-    (fun test_ctxt ->
-      todo "Getting set up...";
-      assert_bool "testing" false);
-  ]
+  "parser_tests" >:::
+  (List.append
+     (List.map accepted_programs
+        ~f:(fun (name, prog) ->
+            name >::
+            (fun _ ->
+               parse_program prog)))
+
+     (List.map rejected_programs
+        ~f:(fun (name, prog) ->
+            name >::
+            (fun _ ->
+               let f = fun () -> parse_program prog in
+               assert_raises Parser.Error f
+                 ~msg:(Printf.sprintf "Expected program '%s' to not parse!" name)))))
 ;;
